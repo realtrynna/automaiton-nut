43c2f949d74aa5b214ebd6d2195b809f0c0c71e1
realtrynna <realtrynna@gmail.com>
2023-01-23 20:54:38 +0900

    Fix: import update

--- app.ts
+++ app.ts
@@ -1,21 +1,24 @@
-import { mouse, Point, straightTo, Key, keyboard, Button } from "@nut-tree/nut-js";
+import {   
+    mouse,
+    Point,
+    screen,
+    singleWord,
+    sleep,
+    useConsoleLogger,
+    ConsoleLogLevel,
+    straightTo,
+    centerOf,
+    keyboard,
+    Key,
+    Button,
+    getActiveWindow, } from "@nut-tree/nut-js";
+// import { preloadLanguages,
+//     Language,
+//     LanguageModelType,
+//     configure, } from "@nut-tree/plugin-ocr"
 
 (async () => {
-    const currentPosition = await mouse.getPosition();
+    // await getActiveWindow()
 
-    // console.log(currentPosition);
-
-    const target = new Point(19, 86);
-
-    await mouse.move(straightTo(target));
-
-    await mouse.click(Button.LEFT);
-
-    const target2 = new Point(670, 185);
-
-    await mouse.move(straightTo(target2));
-
-    await mouse.click(Button.LEFT);
-
-    await keyboard.pressKey(Key.LeftSuper, Key.E);
+    await sleep(5000);
 })();
